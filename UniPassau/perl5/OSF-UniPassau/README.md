5315fc0b7d0a84780fc17eba7057d322b52f4bc5
Mark Overmeer <mark@overmeer.net>
2021-07-13 14:18:57 +0200

    init UniPassau

---
+++ UniPassau/perl5/OSF-UniPassau/README.md
@@ -0,0 +1,10 @@
+# University of Passau, Germany
+
+OSF Tech Lead Michael Granitzer [[michael.granitzer@uni-passau.de]]
+has a few students which implement information extract about texts.
+
+## Bavarian Food
+
+PhD student Mehdi [[mehdi.benamor@uni-passau.de]] wants to collect
+websites about food in Bavaria (Bayern, Germany)  He starts with an
+extract of pages about Bavaria, without restrictions for food.
---
+++ UniPassau/perl5/OSF-UniPassau/lib/OSF/UniPassau/Task/BavarianFood.pm
@@ -0,0 +1,148 @@
+package OSF::UniPassau::Task::BavarianFood;
+use parent 'OSF::Pipeline::Task';
+
+use warnings;
+use strict;
+use utf8;
+
+use charnames ':full', ':alias' => {
+   a_CIRCUM   => 'LATIN SMALL LETTER A WITH CIRCUMFLEX',
+   o_CIRCUM   => 'LATIN SMALL LETTER O WITH CIRCUMFLEX',
+   u_CIRCUM   => 'LATIN SMALL LETTER U WITH CIRCUMFLEX',
+   CIRCUMFLEX => 'COMBINING CIRCUMFLEX ACCENT',
+};
+
+use HTTP::Status       qw(is_success);
+
+my $collect       = $ENV{UP_COLLECT}
+    or die "Environment variable UP_COLLECT missing";
+
+my @content_types = qw(text/html text/xhtml);
+my @domain_names  = qw(bayern);
+
+my @simple_city_names = qw/
+  Amberg
+  Ansbach
+  Aschaffenburg
+  Augsburg
+  Bamberg
+  Bayern
+  Bayreuth
+  Coburg
+  Dachau
+  Deggendorf
+  Erding
+  Erlangen
+  Forchheim
+  Freising
+  Germering
+  Ingolstadt
+  Kaufbeuren
+  Kempten
+  Kitzingen
+  Kulmbach
+  Landshut
+  Lindau
+  Marktredwitz
+  Memmingen
+  Passau
+  Regensburg
+  Rosenheim
+  Schwabach
+  Schweinfurt
+  Straubing
+ /;
+
+my _a_uml  = "(?:ae|a\N{CIRCUMFLEX}|\N{a_CIRCUM})";
+my _o_uml  = "(?:oe|o\N{CIRCUMFLEX}|\N{o_CIRCUM})";
+my _u_uml  = "(?:ue|u\N{CIRCUMFLEX}|\N{u_CIRCUM})";
+my _in_der = "(?:\s+in\s+der\s+|\s+i\.?\s*d\.?\s*|\s*i\s*/\s*d\s+|\s*/\s*)";
+my _an_der = "(?:\s+an\s+der\s+|\s+a\.?\s*d\.?\s*|\s*a\s*/\s*d\s+|\s*/\s*)";
+my _am     = "(?:\s+am?\s+|\s*/\s*)";
+
+my %composed_city_names =
+  ( 'Bad Kissingen'           => qr/\bBad\s+Kissingen\b/,
+  , 'Bad Reichenhall'         => qr/\bBad\s+Reichenhall\b/,
+  , 'Dillingen an der Donau'  => qr/\bDillingen${_an_der}Donau\b/,
+  , 'Dinkelsbühl'             => qr/\bDinkelsb${_u_uml}hl\b/,
+  , 'Donauwörth'              => qr/\bDonauw${_o_uml}rth\b/,
+  , 'Eichstätt'               => qr/\bEichst${_a_uml}tt\b/,
+  , 'Fürstenfeldbruck'        => qr/\bF${_u_uml}rstenfeldbruck\b/,
+  , 'Fürth'                   => qr/\bF${_u_uml}rth\b/,
+  , 'Günzburg'                => qr/\nG${_u_uml}nzburg\b/,
+  , 'Landsberg am Lech'       => qr/\bLandsberg${_am}Lech\b/,
+  , 'München'                 => qr/\bM${_u_uml}nchen\b/,
+  , 'Neuburg an der Donau'    => qr/\bNeuburg${_an_der}Donau\b/,
+  , 'Nürnberg'                => qr/\bN${_u_uml}rnberg\b/,
+  , 'Weiden in der Oberpfalz' => qr/\bWeiden${_in_der}Oberpfalz\b/,
+  , 'Würzburg'                => qr/\bW${_u_uml}rzburg\b/,
+  );
+
+# Not used yet
+my @simple_food = qw/
+   dessert
+   hauptgerichte
+   restaurant
+   rezepte
+   salate
+   speisekarte
+   suppen
+   vorspeisen
+   /;
+
+# not used yet
+my %composed_food =
+  ( 'getränke'   => qr/\bgetr${_a_uml}nke\b/,
+  , 'menü'        => qr/\bmen${_u_uml}\b/,
+  );
+
+sub init($)
+{   my ($self, $args) = @_;
+    $self->{OKT_save} = OSF::Package::WARC->new(directory => $collect);
+    $self->SUPER::init($args);
+}
+
+sub createFilter()
+{   my $self  = shift;
+    my $size  = $self->filterRequiresText(minimum_words => 300);
+    my $ct    = $self->filterContentType(\@content_types);
+    my $rid   = $self->filterDomain(\@domain_names);
+    my $cities1 = $self->filterFullWords(\@simple_city_names);
+    my $cities2 = $self->filterRegexps(\%composed_city_names);
+
+    sub {
+        my $product = shift;
+
+           is_success($product->responseStatus)
+        && $ct->($product)
+        && $product->language =~ qr/^DEU?$/
+        && $size->($product)
+            or return undef;
+
+        my @hits =
+          ( $cities1->($product)
+          , $cities2->($product)
+          , $rid->($product)
+          );
+
+        @hits ? \@hits : undef;
+    };
+}
+
+sub save($$)
+{   my ($self, $product, $hits) = @_;
+warn "SAVE ", $product->name;
+
+    my $save = $self->{OKT_save};
+    foreach my $component ( qw/request response text/ )
+    {   my $part = $product->part($component) or next;
+        $save->addFile($product, "$component.warc-record.gz", $part->refBytes);
+    }
+
+    $save->addJson($product, 'facts.json', +{
+        hits   => $hits,
+        origin => $product->origin,
+    });
+}
+
+1;
