dc1e41fa4cdc5b03de05f39a5f1bf6aead1723e9
Красимир Беров <berov@cpan.org>
2021-07-19 11:02:31 +0300

    Issue #4 OHI to HI;$args is mandatory

--- Pipeline/perl5/OSF-HTML/lib/HTML/Inspect.pm
+++ Pipeline/perl5/OSF-HTML/lib/HTML/Inspect.pm
@@ -13,22 +13,15 @@ use URI();
 use Log::Report 'html-inspect';
 
 # Initialises an HTML::Inspect instance and returns it.
-sub _init ($self, $args = {}) {
-### $args is not optional
+sub _init ($self, $args) {
     my $html_ref_re = qr!\<\s*/?\s*\w+!;
     my $html_ref    = $args->{html_ref} or panic "no html";
-    ref $html_ref eq 'SCALAR'  or error "Not SCALAR";
-### Use 'panic' for internal software errors: it gives a stack-trace
-### like confess which is useful for debugging.
-    $$html_ref =~ $html_ref_re or error "Not HTML";
-    $args->{request_uri} || error '"request_uri" is mandatory';
-### To be consistent with the line before it, I would use 'or'
+    ref $html_ref eq 'SCALAR'  or panic "Not SCALAR";
+    $$html_ref =~ $html_ref_re or panic "Not HTML";
+    $args->{request_uri} or panic '"request_uri" is mandatory';
 
     # use a normalized version
-    $self->{request_uri} = URI->new($args->{request_uri})->canonical;
-### The 'request_uri' is a good name for the parameter, but not for
-### the base in new_abs(), because that is influenced by other parameters
-### like <base>.
+    $self->{HI_request_uri} = URI->new($args->{request_uri})->canonical;
 
     # Translate all tags to lower-case, because libxml is case-
     # sensisitive, but HTML isn't.  This is not fail-safe.
@@ -45,7 +38,7 @@ sub _init ($self, $args = {}) {
 ### This is really ugly formatting.  Can we just indent this with 4, like
 ### we do with code>
 
-    $self->{OHI_doc} = $dom->documentElement;
+    $self->{HI_doc} = $dom->documentElement;
 ### As you can see, I use OHI_ before the object attributes.  This keeps
 ### poeple from "accidentally" do "$inspect->{request_uri}" where the
 ### intention was to write "inspect->requestURI".
@@ -63,7 +56,7 @@ sub new { _init((bless {}, shift), {@_}) }
 # A read-only getter for the parsed document. Returns instance of
 # XML::LibXML::Element, representing the root node of the document and
 # everything in it.
-sub doc { return $_[0]->{OHI_doc} }
+sub doc { return $_[0]->{HI_doc} }
 
 # attributes must be treated as if they are case-insensitive
 sub _attributes ($self, $element) {
@@ -90,7 +83,7 @@ sub _attributes ($self, $element) {
 # ignored here.
 
 sub collectMeta ($self, %args) {
-    return $self->{OHI_meta} if $self->{OHI_meta};
+    return $self->{HI_meta} if $self->{HI_meta};
     my %meta;
     foreach my $meta ($self->doc->findnodes('//meta')) {
         my $attrs = $self->_attributes($meta);
@@ -107,7 +100,7 @@ sub collectMeta ($self, %args) {
             $meta{charset} = $charset;
         }
     }
-    return $self->{OHI_meta} = \%meta;
+    return $self->{HI_meta} = \%meta;
 }
 
 # Collects all meta elements which have an attribute 'property'
@@ -117,15 +110,15 @@ sub collectMeta ($self, %args) {
 # See also: https://developers.facebook.com/docs/sharing/webmasters/crawler
 # https://developers.facebook.com/docs/sharing/webmasters/optimizing
 sub collectOpenGraph ($self, %args) {
-    return $self->{OHI_og} if $self->{OHI_og};
-    $self->{OHI_og} = {};
+    return $self->{HI_og} if $self->{HI_og};
+    $self->{HI_og} = {};
     for my $meta ($self->doc->findnodes('//meta[@property]')) {
 ### Please use foreach() here.
 ### Is findnodes really faster than what I used?
         $self->_handle_og_meta($meta);
     }
 
-    return $self->{OHI_og};
+    return $self->{HI_og};
 }
 
 # A dummy, initial implementation of collecting OG data from a page
@@ -136,21 +129,21 @@ sub _handle_og_meta ($self, $meta) {
 
     # Handle Types title,type,url
     if ($type =~ /^(?:title|type|url)$/i) {
-        $self->{OHI_og}{$ns}{$type} = $attrs->{content} =~ s/\s+/ /gr;
-### We use $self->{OHI_og}{$ns} everywhere, so assign it to a my()
+        $self->{HI_og}{$ns}{$type} = $attrs->{content} =~ s/\s+/ /gr;
+### We use $self->{HI_og}{$ns} everywhere, so assign it to a my()
         return;
     }
 
     # Handle objects, represented as arry of alternatives.
     # A new object starts.
-    if (! exists $self->{OHI_og}{$ns}{$type}) {
-        $self->{OHI_og}{$ns}{$type} = [{$attr => $attrs->{content}}];
+    if (! exists $self->{HI_og}{$ns}{$type}) {
+        $self->{HI_og}{$ns}{$type} = [{$attr => $attrs->{content}}];
 ### No cleanout of content?
         return;
     }
 
     # Continue adding properties to this object.
-    my $arr = $self->{OHI_og}{$ns}{$type};
+    my $arr = $self->{HI_og}{$ns}{$type};
     if (!exists $arr->[-1]{$attr}) {
         $arr->[-1]{$attr} = $attrs->{content};
     }
@@ -190,22 +183,22 @@ sub tag2attr {
 # and values an array of links found in such tags and attributes.
 # TODO: guess the <base> of the document.
 sub collectLinks ($self) {
-    return $self->{OHI_links} if $self->{OHI_links};
+    return $self->{HI_links} if $self->{HI_links};
     while (my ($tag, $attr) = each %{$self->tag2attr}) {
         for my $link ($self->doc->findnodes("//$tag\[\@$attr\]")) {
 ### foreach
 ### attributes must be handled case-insensitive: use _attributes()
 
             # https://en.wikipedia.org/wiki/URI_normalization maybe some day
-            push @{$self->{OHI_links}{"${tag}_$attr"} //= []},
+            push @{$self->{HI_links}{"${tag}_$attr"} //= []},
 ###  //= []  is not needed: autovifivication
-### $self->{OHI_links} is used a lot, assign it a my() before while()
-              URI->new_abs($self->_attributes($link)->{$attr}, $self->{request_uri});
+### $self->{HI_links} is used a lot, assign it a my() before while()
+              URI->new_abs($self->_attributes($link)->{$attr}, $self->{HI_request_uri});
 ### You use $self->{request_uri} for any link, and there are always many
 ### links.  Before while() use $self->base to the URI.
         }
     }
-    return $self->{OHI_links};
+    return $self->{HI_links};
 }
 
 
