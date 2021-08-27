1ab6b6cf3416959a851cf56c3f8ffad41b35eaa0
Mark Overmeer <mark@overmeer.net>
2021-08-27 16:45:14 +0200

    HTML::Inspect better normalization

--- Pipeline/perl5/OSF-HTML/lib/HTML/Inspect/Util.pm
+++ Pipeline/perl5/OSF-HTML/lib/HTML/Inspect/Util.pm
@@ -9,6 +9,7 @@ our @EXPORT_OK = qw(trim_attr xpc_find get_attributes absolute_url);
 use Log::Report 'html-inspect';
 
 use URI::Fast qw(html_url);
+use URI       ();
 
 # Deduplicate white spaces and trim string.
 sub trim_attr($) { ($_[0] // '') =~ s/\s+/ /grs =~ s/^ //r =~ s/ \z//r }
@@ -40,12 +41,45 @@ sub get_attributes($) {
 
 # function absolute_url($relative_url, $base)
 # Convert a (possible relatative) url into an absolute version.  Things
-# which are not urls (data: and javascript:) will return nothing.
+# which are not urls will return nothing.
+
+# See https://github.com/Skrodon/temporary-documentation/wiki/HTML-link-statics
+# to see what we try to clean-up here.
+
+my %take_schemes = map +($_ => 1), qw/mailto http https ftp tel/;
 
 sub absolute_url($$) {
-    my $href = $_[0] || 'x';  #XXX avoid crash in URI::Fast 0.52
-    my $ref  = html_url($href, $_[1])->as_string;
-    $ref =~ m/^(?:data\:|javascript\:)/ ? () : $ref;
+    my ($href, $base) = @_;
+
+    my $scheme = $href =~ /^([a-z]+)\:/i ? lc($1) : 'https';  # base always http*
+    $take_schemes{$scheme} or return ();
+
+    return URI->new_abs($href, $base)->canonical->as_string
+        unless $scheme eq 'https' || $scheme eq 'http';  # about 2.2% of the input
+
+    ### For http*, URI::Fast::html_url is mainly better than URI.  And much, much
+    #   faster.  Even when some things need to be fixed later.
+
+
+    # URI::Fast does not remove empty fragments and queries
+#   my $url = html_url($href =~ s/\#\z//r =~ s/\?\z//r, $base);
+
+#XXX avoid crash in URI::Fast 0.52
+$href =~ s/\#\z//;
+$href =~ s/\?\z//;
+$href ||= 'x';
+my $url = html_url($href, $base);
+
+    if(my $port = $url->port) {
+        $port =~ m/^[0-9]{1,8}$/ or return ();  # not validated by URI::Fast
+        $url->port(undef)                       # not normalized by URI::Fast
+            if $port == ($scheme eq 'http' ? 80 : 432);
+    }
+
+    #TODO: IDN on host
+    #TODO: utf8->hex on path.  See xt/benchmark_utf8.t
+
+    $url->as_string;
 }
 
 1;
